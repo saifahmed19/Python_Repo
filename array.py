114ba47e74f93a2759ed574fad2ad9eafb8d217a
SAIF AHMED KHAN <42868166+saifahmed19@users.noreply.github.com>
2021-07-31 02:22:53 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,27 @@
+heros=['spider man','thor','hulk','iron man','captain america']
+'''
+Using this find out,
+1. Length of the list
+2. Add 'black panther' at the end of this list
+3. You realize that you need to add 'black panther' after 'hulk',
+   so remove it from the list first and then add it after 'hulk'
+4. Now you don't like thor and hulk because they get angry easily :)
+   So you want to remove thor and hulk from list and replace them with doctor strange (because he is cool).
+   Do that with one line of code.
+5. Sort the heros list in alphabetical order (Hint. Use dir() functions to list down all functions available in list)
+'''
+#1
+print(len(heros))
+#2
+heros.append('black panther')
+print("updeted list",heros)
+#3
+heros.remove('black panther')
+heros.insert(3, 'black panther')
+print('list: ',heros)
+#4
+heros[1:3] = ['doctor strange']
+print(heros)
+#5
+heros.sort()
+print('sortd list', heros)
